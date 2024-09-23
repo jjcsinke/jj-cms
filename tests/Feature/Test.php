e595fbd2b4ed79b8b7509bfb4dd40d9f14f43ce3
jan-jaap <jan-jaap.sinke@tover.care>
2024-09-23 23:25:29 +0200

    starting a cms repo

---
+++ tests/Feature/Test.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace JJCS\Tests\Feature;
+
+use JJCS\CMS\Providers\CmsServiceProvider;
+use JJCS\Tests\TestCase;
+
+class Test extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [CmsServiceProvider::class];
+    }
+    
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200);
+    }
+}
